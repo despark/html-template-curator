c681a1def81b9e32195602bf798f5e2231824d0b
Stoyan Dipchikov <sdipchikov@despark.cpm>
2015-08-07 16:54:45 +0300

    Removed some config (whups) and style update

--- src/config/config.php
+++ src/config/config.php
@@ -12,7 +12,7 @@ return [
     'upload_directory_name' => 'upload',
 
     'soundcloud' => [
-        'client_id'     => 'b1d01e4c0fc396568357747a6e2e9c19',
-        'client_secret' => '55abe4326760c4f4894271ec887b88be'
+        'client_id'     => '',
+        'client_secret' => ''
     ],
 ];
